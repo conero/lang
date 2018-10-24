517bee80addc1fbfcf3d5f5d6d0b7b62339d69c2
Joshua Conero <brximl@163.com>
2018-10-25 00:49:21 +0800

    go leetCode add-two-numbers 编写成功

--- hi-golang/src/leetcode/add-two-numbers2.go
+++ hi-golang/src/leetcode/add-two-numbers2.go
@@ -1,34 +1,40 @@
 package main
 
 /**
-	@date 2018年10月24日 星期三
-	@link
- */
+@date 2018年10月24日 星期三
+@link
+*/
 
 import (
 	"fmt"
+	"strconv"
 )
+
 // 链表
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
 // 获取值
-func (ln *ListNode) Value() int {
-	var num int
-	b := 1
-	for tnd := ln; tnd != nil;{
-		num += b * tnd.Val
-		b *= 10
+func (ln *ListNode) Value() int64 {
+	var str string
+	for tnd := ln; tnd != nil; {
+		str += strconv.Itoa(tnd.Val)
 		tnd = tnd.Next
 	}
-	return num
+	n, er := strconv.ParseInt(str, 10, 64)
+	if er != nil {
+		n = 0
+	}
+	return n
 }
+
 // 获取值
 func (ln *ListNode) Value64() int64 {
 	var num int64
 	var b int64 = 1
-	for tnd := ln; tnd != nil;{
+	for tnd := ln; tnd != nil; {
 		num += b * int64(tnd.Val)
 		b *= 10
 		tnd = tnd.Next
@@ -40,23 +46,24 @@ func (ln *ListNode) Value64() int64 {
 func (ln *ListNode) Value2() int {
 	var num int
 	b := 1
-	for tnd := ln; tnd != nil;{
+	for tnd := ln; tnd != nil; {
 		num += b * tnd.Val
 		b *= 10
 		tnd = tnd.Next
 	}
 	return num
 }
+
 /**
 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]		=> ListNode
 [5,6,4]   int(465)
- */
-func GetLNdFromQue(que []int) *ListNode  {
+*/
+func GetLNdFromQue(que []int) *ListNode {
 	var lnd *ListNode
-	for i := len(que) - 1; i> -1; i--{
-		if lnd == nil{
+	for i := len(que) - 1; i > -1; i-- {
+		if lnd == nil {
 			lnd = &ListNode{que[i], nil}
-		}else {
+		} else {
 			tmpLnd := &ListNode{que[i], lnd}
 			lnd = tmpLnd
 		}
@@ -73,7 +80,7 @@ type Case struct {
 //		输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
 //		输出：7 -> 0 -> 8
 //		原因：342 + 465 = 807
-func (c Case) Test1()  {
+func (c Case) Test1() {
 	lnd1 := &ListNode{2, &ListNode{4, &ListNode{3, nil}}}
 	lnd2 := &ListNode{5, &ListNode{6, &ListNode{4, nil}}}
 
@@ -84,10 +91,9 @@ func (c Case) Test1()  {
 	fmt.Println(lnd1.Value(), lnd2.Value2(), atn.Value())
 }
 
-
-func (c Case) Test2()  {
+func (c Case) Test2() {
 	lnd1 := &ListNode{1, &ListNode{0, &ListNode{0,
-	&ListNode{0, &ListNode{0, &ListNode{1, nil}}}}}}
+		&ListNode{0, &ListNode{0, &ListNode{1, nil}}}}}}
 	lnd2 := &ListNode{5, &ListNode{6, &ListNode{4, nil}}}
 
 	//fmt.Println(lnd1.Value())
@@ -97,7 +103,7 @@ func (c Case) Test2()  {
 	fmt.Println(lnd1.Value(), lnd2.Value2(), atn.Value())
 }
 
-func main()  {
+func main() {
 	var cs Case
 	cs.Test1()
 	cs.Test2()
@@ -111,14 +117,11 @@ func main()  {
 	fmt.Println(addTwoNumbers(GetLNdFromQue([]int{1, 8}), GetLNdFromQue([]int{0})).Value())
 	fmt.Println(addTwoNumbers(&ListNode{1, &ListNode{8, nil}}, &ListNode{0, nil}).Value())
 
+	t := addTwoNumbers(&ListNode{1, &ListNode{8, nil}}, nil)
+	fmt.Println(t)
+	//fmt.Println(t, t.Next)
 
 }
-
-
-
-
-
-
 
 /**
  * Definition for singly-linked list.
@@ -142,49 +145,56 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
 	maxLen := len(q1)
 	minLen := len(q2)
-	if minLen > maxLen{
+	if minLen > maxLen {
 		// 值交换
 		maxLen, minLen = minLen, maxLen
 		q1, q2 = q2, q1
 	}
 	//fmt.Println(q1, q2, maxLen, minLen)
 	// 遍历请且生成链表
-	mBit := 0			// 进位
+	mBit := 0 // 进位
 	que := []int{}
-	for i := 1; i <= maxLen; i++{
-		if i <= minLen{
-			for j := 1; j <= minLen; j++{
-				d1 := q1[maxLen - j]
-				d2 := q2[minLen - j]
-				det := d1 + d2 + mBit
-				if det > 9{
-					mBit = 1
-					det = det - 10
-				}else {
-					mBit = 0
-				}
-				que = append(que, det)
-
+	for i := 0; i < maxLen; i++ {
+		v := q1[i]
+		if i < minLen {
+			v += q2[i] + mBit
+			if v > 9{
+				v = v - 10
+				mBit = 1
+			}else {
+				mBit = 0
 			}
-			i = minLen
+			que = append(que, v)
 			continue
 		}
-		//v := q1[maxLen - i]
-		//if mBit > 0{
-		//	v = v + mBit
-		//	mBit = 0
-		//}
-		que = append(que, q1[maxLen - i])
+
+		if mBit > 0 {
+			v = v + mBit
+		}
+
+		if v > 9{
+			v = v - 10
+			mBit = 1
+		}else {
+			mBit = 0
+		}
+
+		que = append(que, v)
 	}
-	if mBit > 0{
+
+	if mBit > 0 {
 		que = append(que, mBit)
 	}
+	//fmt.Println(que)
 	var value *ListNode
-	for j := len(que) - 1; j > -1; j--{
+	//vlen := len(que)
+	//for j := 0; j < vlen; j++{
+	for j := len(que) - 1; j > -1; j-- {
 		v := que[j]
-		if value == nil{
+		//fmt.Println(v, j)
+		if value == nil {
 			value = &ListNode{v, nil}
-		}else {
+		} else {
 			value = &ListNode{v, value}
 		}
 	}
