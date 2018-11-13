d87c465d6e61677f2ec3f324cddba95944862210
Joshua Conero <brximl@163.com>
2018-11-13 10:46:21 +0800

    添加 c 语言脚本

---
+++ hi-c/compiler.md
@@ -0,0 +1,12 @@
+# compiler
+>
+> 2018年11月13日 星期二
+
+
+## gcc
+
+```ini
+# 文件编译 <file> -> <name>
+$ gcc <file> -o <name>
+$ ./<name>
+```
---
+++ hi-c/hw.c
@@ -0,0 +1,11 @@
+/**
+ *  2018年11月13日 星期二
+ *  c to helo world
+**/
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    printf("%d", 5*8);
+    return 0;
+}
