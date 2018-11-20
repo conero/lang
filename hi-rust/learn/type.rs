72ebae3d3bb8acba3b2f52b9aa81764e2fa8de45
Joshua Conero <brximl@163.com>
2018-11-20 23:22:55 +0800

    rust 测试脚本优化

--- hi-rust/learn/type.rs
+++ hi-rust/learn/type.rs
@@ -1,9 +1,23 @@
 // 20181116
 // data-type learn
 // Joshua Conero  > type.rs
+use std::env;
 
+fn tArray(){}
 
 fn main(){
     println!("Rust Data-Type Learning");
+    let mut ctt = 0;
+    for arg in env::args(){
+        ctt += 1;
+        if ctt == 1{
+            continue;
+        }
+        match arg {
+            "array" => {
+                tArray();
+            }
+        }
+    }
 }
 
