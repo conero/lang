7c9b7e8e192f8f103af83d6c7599b16da7c98b4f
Joshua Conero <brximl@163.com>
2018-12-11 15:11:38 +0800

    学习 rust 宏

--- hi-rust/learn/cargo/src/main.rs
+++ hi-rust/learn/cargo/src/main.rs
@@ -4,16 +4,3 @@
 // 单文件写入
 //-------
 
-// 20181209
-// Rust 与 C 交互；生成 dll/so 等
-// Joshua Coner
-
-
-#![allow(unused_variables)]
-fn main() {
-    // extern 的使用无需 unsafe。
-    #[no_mangle]
-    pub extern "C" fn call_from_c() {
-        println!("Just called a Rust function from C!");
-    }
-}
