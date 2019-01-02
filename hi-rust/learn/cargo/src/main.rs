cc8e6e96b0363b73474483e811907c51865d3bf7
Joshua Conero <brximl@163.com>
2019-01-02 14:24:46 +0800

    optimize the Rust for ownership learning

--- hi-rust/learn/cargo/src/main.rs
+++ hi-rust/learn/cargo/src/main.rs
@@ -19,8 +19,25 @@ fn main(){
     println!("c = {}", s);
 
     // 堆上数据操作
+    // 变量与数据交互的方式（一）：移动
     let sv = String::from("String 字符串");
-    let sv1 = sv;
+    let sv1 = sv;   // sv -> sv1
     println!("sv1 = {}", sv1);
-    println!("sv = {}", sv);
+    // println!("sv = {}", sv);    // 变量已被借用  error[E0382]: borrow of moved value: `sv`
+
+    // 变量与数据交互的方式（二）：克隆
+    let sv = String::from("String 字符串： 隐藏");
+    let sv1 = sv.clone();
+    println!("{}, {}", sv1, sv);
+    // sv =>
+    let sv2 = get_str_onwership(sv);
+    //println!("{}, {}", sv2, sv);    // error[E0382]: borrow of moved value: `sv`
+    println!("{}", sv2);
+}
+
+// 值获取
+fn get_str_onwership(bs: String) -> String{
+    println!(" get the string dsta [{}]", bs);
+    let bs = bs + &" Maker".to_string();
+    bs
 }
