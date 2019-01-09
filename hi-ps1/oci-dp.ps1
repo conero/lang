16648f224bf43449a96b32b4c250c2b6304dc961
Joshua Conero <brximl@163.com>
2019-01-09 14:46:21 +0800

    ps 脚本优化

--- hi-ps1/oci-dp.ps1
+++ hi-ps1/oci-dp.ps1
@@ -1,5 +1,6 @@
 # Oracle 数据导入导出
 # 2019年1月9日 星期三
+# 参考 link+ https://blog.csdn.net/jiandonghan/article/details/81259093
 
 
 $isExp = Read-Host '数据导出(Y/N)？'
@@ -15,6 +16,10 @@ $dumpfile = $dbname+'.dmp'
 $schemas = $user
 
 
+# 数据导入
+# test1 => test2; 前面用户导入到后面的
+# impdp system/password@orcl dumpfile=test.dmp remap_schema=test1:test2
+# dmp 文件: ~/admin/dbname/dpdump
 
 # expdp dev/zotion123@mci600a dumpfile=mci600a-dev.dmp schemas=sa
 # expdp $user/$password@$dbname dumpfile=$dumpfile schemas=$schemas
