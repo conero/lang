96dacdef71607a329f8fd6fa0ba790fc145d34d9
conero <conero@163.com>
2019-04-03 23:16:13 +0800

    go leetcode 添加题目

---
+++ hi-golang/src/leetcode/add-strings.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 解答
+// @TODO StillNeedToDo
+/*
+	给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+	注意：
+		num1 和num2 的长度都小于 5100.
+		num1 和num2 都只包含数字 0-9.
+		num1 和num2 都不包含任何前导零。
+		你不能使用任何內建 BigInteger 库， 也不能直接将输入的字符串转换为整数形式。
+ */
+func addStrings(num1 string, num2 string) string {
+
+}
+
+
+// 控制台
+func main() {
+
+}
