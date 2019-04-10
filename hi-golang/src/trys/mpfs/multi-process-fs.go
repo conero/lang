4797594c889e54c76c066e36398e9e8ebebc6a6b
conero <conero@163.com>
2019-04-10 22:52:43 +0800

    go/trys/mpfs 完善 info 信息，以及对应的结构

--- hi-golang/src/trys/mpfs/multi-process-fs.go
+++ hi-golang/src/trys/mpfs/multi-process-fs.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"crypto/sha1"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
 	"strings"
+	"time"
 )
 
 const (
@@ -33,10 +36,26 @@ type Mpfs struct {
 // 树字典
 type TreeDickData struct {
 	TreePath string // 路径标识符
+	Depth    int    // 深度
+	IsDir    bool
+	Size     int64
+	ModTime  time.Time // 修改时间
 }
 type HashCode string
 type TreeDick map[HashCode]TreeDickData
 
+// info 返回的数据格式
+type MpInfoData struct {
+	Td        TreeDick // 数据字典
+	DirCount  int
+	FileCount int
+	Size      int64
+	Dir       string
+	Depth     int
+	RunTimes  string
+}
+
+// 构造函数
 func NewMpfs() *Mpfs {
 	mpfs := &Mpfs{}
 	if vdir, has := Data[KeyCmdDir]; has {
@@ -46,12 +65,21 @@ func NewMpfs() *Mpfs {
 	return mpfs
 }
 
-func TD2(dir string) TreeDick {
+// 简单树形字典生成器
+// 单线程数据统计
+func TD2(dir string) MpInfoData {
+	rTm := getTime()
+	var infoDt MpInfoData = MpInfoData{
+		DirCount:  0,
+		FileCount: 0,
+		Size:      0,
+	}
 	var td TreeDick
 	td = map[HashCode]TreeDickData{}
 
 	// 标准化地址
 	dir = strings.Replace(dir, "\\", "/", -1)
+	infoDt.Dir = dir
 	dirLen := len(dir)
 	if dirLen > 3 && dir[dirLen-1:] != "/" {
 		dir += "/"
@@ -67,20 +95,33 @@ func TD2(dir string) TreeDick {
 		for _, ds := range d1Src {
 			pRaw := d1 + ds.Name()
 			pCode := strings.Replace(pRaw, dir, "", -1)
-			td[HashCode(getHash1(pCode))] = TreeDickData{pCode}
-
+			vdepth := strings.Count(pCode, "/") + 1
+			td[HashCode(getHash1(pCode))] = TreeDickData{
+				TreePath: pCode,
+				Depth:    vdepth,
+				IsDir:    ds.IsDir(),
+				Size:     ds.Size(),
+				ModTime:  ds.ModTime(),
+			}
+			infoDt.Depth = int(math.Max(float64(infoDt.Depth), float64(vdepth)))
 			//fmt.Println(pRaw, pCode, ds.IsDir(), dir)
 			// 递归
 			if ds.IsDir() {
+				infoDt.DirCount += 1
 				pRaw += "/"
 				todoReader(pRaw)
+			} else {
+				infoDt.FileCount += 1
+				infoDt.Size += ds.Size()
 			}
 
 		}
 	}
 
 	todoReader(dir)
-	return td
+	infoDt.Td = td
+	infoDt.RunTimes = rTm()
+	return infoDt
 }
 
 // 命令路由器
@@ -93,14 +134,22 @@ func (m *Mpfs) Router() {
 
 // 文件信息
 func (m *Mpfs) Info() {
-	fmt.Println(TD2(m.Dir))
+	td2 := TD2(m.Dir)
+	//fmt.Println(td2)
+	tClk := getTime()
+	js, _ := json.Marshal(td2)
+	fmt.Println(string(js))
+	fmt.Println(tClk())
 }
 
-var maxProceeX int = 100
+var maxProceeX int = 100 // 最大的进程数
 var action string
 var Data map[string]string
+var globalTmer func() string
+
 
 func init() {
+	globalTmer = getTime()
 	// 命令解析
 	osArgsLen := len(os.Args)
 	Data = map[string]string{}
@@ -126,11 +175,23 @@ func getHash1(s string) string {
 	return fmt.Sprintf("%x", h1.Sum([]byte(s)))
 }
 
+// 获取时间差
+func getTime() func() string {
+	tmStart := time.Now()
+	return func() string {
+		d := time.Since(tmStart)
+		return fmt.Sprintf("%s", d)
+	}
+}
+
 func main() {
 	// 测试
 	action = "info"
 	Data["dir"] = "D:/tmp/"
+	//Data["dir"] = "D:/"
+	Data["dir"] = "D:/tmp"
 
+	// 入口配置
 	mpsf := NewMpfs()
 	mpsf.Router()
 
@@ -140,4 +201,7 @@ func main() {
 	//fmt.Println(
 	//	getHash1("1"),
 	//	" ", getHash1("2"))
+
+	// 时间
+	fmt.Println(".  程序运行的全部时间：", globalTmer())
 }
