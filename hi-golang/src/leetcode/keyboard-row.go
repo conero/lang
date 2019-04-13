846a3d12df0163c26a741755ea121def5d4363be
conero <conero@163.com>
2019-04-14 00:12:28 +0800

    go leetcode 题目实现

--- hi-golang/src/leetcode/keyboard-row.go
+++ hi-golang/src/leetcode/keyboard-row.go
@@ -1,6 +1,9 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 /**
  * @DATE        2019/4/13
@@ -8,6 +11,8 @@ import "strings"
  * @DESCRIPIT   500. 键盘行
  * @LINK		https://leetcode-cn.com/problems/keyboard-row/
  **/
+
+// @TODO 完善题目信息，算法已解决
 
 // 解答
 func findWords(words []string) []string {
@@ -19,10 +24,27 @@ func findWords(words []string) []string {
 	wordValue := []string{}
 	for _, s := range words{
 		var idx int = -1
+		isBreak := false
 		for _, w := range strings.Split(s, ""){
 			w = strings.ToUpper(w)
-
+			for i, comW := range dick{
+				if strings.Index(comW, w) > -1{
+					if idx == -1{
+						idx = i
+					}else if i != idx{
+						isBreak = true
+						break
+					}
+				}
+			}
+			if isBreak {
+				break
+			}
 		}
+		if isBreak {
+			continue
+		}
+		wordValue = append(wordValue, s)
 	}
 	return wordValue
 
@@ -31,6 +53,7 @@ func findWords(words []string) []string {
 
 // 控制台
 func main() {
+	fmt.Println(findWords([]string{"Hello", "Alaska", "Dad", "Peace"}))
 }
 
 
