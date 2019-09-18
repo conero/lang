675c5b26c129d9df34b434966c3ab8b2d0e946ee
Conero <conero@163.com>
2019-09-18 17:38:28 +0800

    python: 环境变量修改，软修改

--- hi-python/trys/common/cmd.py
+++ hi-python/trys/common/cmd.py
@@ -8,6 +8,7 @@ import time
  Cmds 类型注册使用
  ```python
     # 实例化项目
+    # 也可 Cmds 进行充重置
     cmd = Cmds()
     
     # 注册命令
@@ -16,38 +17,63 @@ import time
         cmd.quit()
         
     cmd.on('exit', exitCmd)
-    
+    # 通用类
+    class PartyDemo:
+        pass
+    # 继承 Party 类，可是现在二级默认调用以及二级未知检测
+    class PartyDemo2(Party):
+        pass
+        
+    cmd.party(PartyDemo)
+    cmd.party(PartyDemo2)
+    # [$ cmd subCmd] 
+    cmd.party(PartyDemo, "cmd")
+    cmd.party(PartyDemo2, "cmd")
  ```
 '''
 
 # 常量
 PARTY_CMD_DEFAULT = 'cmdDefault'
-PARTY_CMD_UNFIND =  'cmdUnfind'
+PARTY_CMD_UNFIND = 'cmdUnfind'
+
+
+# 方法存在性检测
+def methodExist(obj, method):
+    has, mth = False, None
+    try:
+        if callable(getattr(obj, method)):
+            has = True
+            mth = getattr(obj, method)
+    finally:
+        return has, mth
+
 
 # 通用的命令行解析类
 class Cmds:
     def __init__(self, args=None):
         self.ctime = time.time()  # 当前的时间
         self.args = args
-        self.cmd = ''  # 命令
-        self.subCmd = ''  # 子命令
-        self.data = {}  # 数据
+        self._quitMk = False  # 退出描述
         self.onKvQue = {}  # 命令配置
-        self.setting = []  # 配置
         self.partyData = {  # 类型测试
             "cmds": {},
             "instance": []
         }
-        self._quitMk = False  # 退出描述
+        self._init_data()
+
+    # 初始化数据
+    def _init_data(self):
+        self.cmd = ''  # 命令
+        self.subCmd = ''  # 子命令
+        self.data = {}  # 数据
+        self.setting = []  # 配置
 
     # 命令行解析
     def router(self, args=None):
-        self.cmd = None  # 命令
-        self.data = {}  # 数据
-        if args == None:
+        self._init_data()
+        if args is None:
             args = self.args
-
-        if args == None:
+        if args is None:
             self.empty()
         else:
             args = args.split(" ")
@@ -62,12 +88,11 @@ class Cmds:
                         idx = arg.index('=')
                         self.data[arg[:idx + 1]] = arg[idx + 1:]
                     else:
-                        self.setting.append(arg[2:])
+                        self.setting.append(arg)
                 # -xzy => -x -z -y  无效后顺序
                 elif arg[:1] == "-":
                     arg = arg[1:]
-                    argQue = arg.split('')
-                    for v in argQue[:]:
+                    for v in arg:
                         self.setting.append(v)
                 elif self.cmd is None or self.cmd == '':
                     self.cmd = arg
@@ -109,26 +134,31 @@ class Cmds:
         # subCmd 为空
         if self.subCmd is None or self.subCmd == '':
             for ins in party["instance"][:]:
-                if callable(getattr(ins, self.cmd)):
-                    getattr(ins, self.cmd)()
+                has, mth = methodExist(ins, self.cmd)
+                if has:
+                    mth()
                     return True
 
         if self.cmd in party["cmds"]:
             ins = party["cmds"][self.cmd]
 
             if self.subCmd is None or self.subCmd == '':
-                if callable(getattr(ins, PARTY_CMD_DEFAULT)):
-                    getattr(ins, PARTY_CMD_DEFAULT)()
+                has, mth = methodExist(ins, PARTY_CMD_DEFAULT)
+                if has:
+                    mth()
                     return True
             elif self.subCmd != '' and self is not None:
                 if self.cmd in party["cmds"]:
                     ins = party["cmds"][self.cmd]
-                    if callable(getattr(ins, self.subCmd)):
-                        getattr(ins, self.subCmd)()
+                    has, mth = methodExist(ins, self.subCmd)
+                    if has:
+                        mth()
                         return True
-                    elif callable(getattr(ins, PARTY_CMD_UNFIND)):
-                        getattr(ins, PARTY_CMD_UNFIND)(self.subCmd)
-                        return True
+                    else:
+                        has, mth = methodExist(ins, PARTY_CMD_UNFIND)
+                        if has:
+                            mth(self.subCmd)
+                            return True
         return False
 
     # 默认的空命令运行
@@ -151,6 +181,21 @@ class Cmds:
     def isQuit(self):
         return self._quitMk
 
+    # 选项检测是否存在
+    def option(self, *args):
+        has = False
+        for arg in args[:]:
+            try:
+                if self.setting.index(arg) >= 0:
+                    has = True
+            except:
+                pass
+
+            if has:
+                break
+        return has
+
+
 # party 执行
 class Party:
     # 默认命令
@@ -159,7 +204,8 @@ class Party:
 
     # 二级命令未知时
     def cmdUnfind(self, cmd=None):
-        print(" ["+cmd+"] 命令不存在")
+        print(" [" + cmd + "] 二级命令不存在")
+
 
 # 简单的应用
 class SimpleApp:
