7f8d3b347680a703b1127d3011d6adaee66a9fe0
Conero <conero@163.com>
2019-09-25 23:52:57 +0800

    py: call_user_func 描述完善

--- hi-python/learning/call_user_func.py
+++ hi-python/learning/call_user_func.py
@@ -1,7 +1,8 @@
 # 2019年9月25日 星期三
 # 实现类似 PHP 中的 call_user_func
 # version - 3.7
-
+# 参考:
+#       http://www.itdaan.com/blog/2009/05/13/992b030eb45c086c6330bba45a76eedf.html
 
 class Aa:
     def __init__(self):
