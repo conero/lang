ab8afbd127ad732da8ca8ca8f6fc639c7f6beb1d
Conero <conero@163.com>
2019-09-26 22:52:09 +0800

    类对象方法

--- hi-python/learning/call_user_func.py
+++ hi-python/learning/call_user_func.py
@@ -5,6 +5,8 @@
 #       http://www.itdaan.com/blog/2009/05/13/992b030eb45c086c6330bba45a76eedf.html
 
 class Aa:
+    # 作用域名称与属性名不可重合
+    vConst = '类内部变量'
     def __init__(self):
         self.name = 'Base class'
 
@@ -18,3 +20,10 @@ class Aa:
 # 测试
 aa = Aa()
 print(getattr(aa, 'age')(16))
+
+print(Aa.vConst)
+Aa.vConst = '外部改写'
+print(Aa.vConst)
+
+# 不存在的方法检测
+print(hasattr(aa, 'hereNoneThisMethod'))
