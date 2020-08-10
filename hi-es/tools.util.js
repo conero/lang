0cc206de823fcf4630b6257e5e54f76bdeb4af0e
Conero <conero@163.com>
2020-08-10 11:14:36 +0800

    ES: ganzhi,zodiac 等年份列表数据获取

--- hi-es/tools.util.js
+++ hi-es/tools.util.js
@@ -3,6 +3,42 @@
  * 
  */
 
+ /**
+ * 字符串清洗
+ * @param {string} str 
+ * @param {array} cleanList 
+ */
+function cleanString(str, cleanList){
+    if ('object' !== typeof cleanList){
+        return str
+    }
+    cleanList.forEach((vque) => {
+        let key = vque.shift();
+        var v = vque[0];
+        if(!v){
+            return;
+        }
+        
+        if('object' !== typeof v){
+            v = `${v}`;
+            v = v.trim();
+            if(v === ''){
+                return;
+            }
+            v = [v];
+        }
+        v.forEach(ts => {
+            ts = ts.trim();
+            if(ts === ''){
+                return;
+            }
+            str = str.replace(new RegExp(ts, 'g'), key)
+        });
+    });
+
+    return str
+}
+
 //@todo 需要修正，如错误解析数据
 //test case: 
 //    生于公元一九五六年十一月二十四日。
@@ -150,32 +186,8 @@ function turnNumByText(text){
         [9, ['九', '玖']], 
     ];
 
-    clearData.forEach((vque) => {
-        if('object' !== typeof vque){
-            return;
-        }
-        let key = vque.shift();
-        var v = vque[0];
-        if(!v){
-            return;
-        }
-
-        if('object' !== typeof v){
-            v = `${v}`;
-            v = v.trim();
-            if(v === ''){
-                return;
-            }
-            v = [v];
-        }
-        v.forEach(ts => {
-            ts = ts.trim();
-            if(ts === ''){
-                return;
-            }
-            text = text.replace(new RegExp(ts, 'g'), key)
-        });
-    });
+    text = cleanString(text, clearData);
+    console.log(text);
 
     // 中文转数字
     let matchQue = text.match(/[0-9]*c[SBQW][0-9]*/g);
@@ -185,6 +197,7 @@ function turnNumByText(text){
             if(ms.indexOf(keyword) > -1){
                 tArr = ms.replace(keyword, sep).split(sep);
                 tArrLen = tArr.length;
+                console.log(tArr);
                 tArr.forEach((s, idx) => {
                     if(s.trim() === ''){
                         tArr[idx] = idx === 0? '1': '0';
@@ -192,7 +205,9 @@ function turnNumByText(text){
                 });
                 ms = tArr.join('');
             }
-            text = text.replace((new RegExp(oMs, 'g')), ms);
+            //@notice 使用`g`标识，可能会替换很多次
+            //text = text.replace((new RegExp(oMs, 'g')), ms);
+            text = text.replace((new RegExp(oMs)), ms);
 
             // @todo 百千暂时未能实现
             // let keyword = "cB", tArr = [], tArrLen = 0, sep = '.', oMs = ms;
@@ -212,7 +227,10 @@ function turnNumByText(text){
     return text;
 }
 
-
+// 测试错误
+// 生公元一九七0年十月二十九日。徙居天柱城。
+// 生公元一九九四年正月十三日。
+// 
 //@todo 需要修正，如错误解析数据
 //test case: 
 //    生于公元一九五六年十一月二十四日。
@@ -230,11 +248,9 @@ function turnNumByText(text){
 function getDateByTextX(text){
     //中文替换英文
     text = text.replace(/\s/g, '');
-    text = turnNumByText(text);
-    //dickMap/Object `for in` key 会进行排序，遂换用数组。
-    const clearData = [
+    text = cleanString(text, [
         [0, ['零', '〇']], 
-        ['01', ['初一', '正月']],
+        ['01', ['初一']],
         ['02', ['初二']],
         ['03', ['初三']],
         ['04', ['初四']],
@@ -244,15 +260,12 @@ function getDateByTextX(text){
         ['08', ['初八']],
         ['09', ['初九']],
         ['10', ['初十']],
-        ['11', ['十一']],
-        ['12', ['十二', '腊月']],
-        ['13', ['十三']],
-        ['14', ['十四']],
-        ['15', ['十五']],
-        ['16', ['十六']],
-        ['17', ['十七']],
-        ['18', ['十八']],
-        ['19', ['十九']],
+    ]);
+    text = turnNumByText(text);
+    //dickMap/Object `for in` key 会进行排序，遂换用数组。
+    const clearData = [
+        ['12-', ['腊月']],
+        ['01-', ['正月']],
         [1, ['一', '壹']], 
         [2, ['二', '贰']], 
         [3, ['三', '叁']], 
@@ -342,22 +355,24 @@ function getDateByTextX(text){
 // 干支运算
 const GanOrder = ['甲', '乙', '丙', '丁', '戊', '己', '庚', '辛', '壬', '癸'];
 const ZhiOrder = ['子', '丑', '寅', '卯', '辰', '巳', '午', '未', '申', '酉', '戌', '亥'];
+const GzSpanceYear = 60;        //干支间距年
+const ZodiacSpanceYear = 12;
 class GanZhi{
-    #_gzList = [];  //干支缓存
-    #_gOrder = [];
-    #_zOrder = [];
-    #_zodiac = [];
-    #_ganZodiacMap = {};        //干支生肖字典
+    __gzList = [];  //干支缓存
+    __gOrder = [];
+    __zOrder = [];
+    __zodiac = [];
+    __ganZodiacMap = {};        //干支生肖字典
     constructor(){
         // 实际的GZ顺序
-        this.#_gOrder = ['庚', '辛', '壬', '癸', '甲', '乙', '丙', '丁', '戊', '己'];
-        this.#_zOrder = ['申', '酉', '戌', '亥', '子', '丑', '寅', '卯', '辰', '巳', '午', '未'];
+        this.__gOrder = ['庚', '辛', '壬', '癸', '甲', '乙', '丙', '丁', '戊', '己'];
+        this.__zOrder = ['申', '酉', '戌', '亥', '子', '丑', '寅', '卯', '辰', '巳', '午', '未'];
         // 子-鼠，丑-牛，寅-虎，卯-兔，辰-龙，巳-蛇， 午-马，未-羊，申-猴，酉-鸡，戌-狗，亥-猪。
-        this.#_zodiac = ['鼠', '牛', '虎', '兔', '龙', '蛇', '马', '羊', '猴', '鸡', '狗', '猪'];
+        this.__zodiac = ['鼠', '牛', '虎', '兔', '龙', '蛇', '马', '羊', '猴', '鸡', '狗', '猪'];
 
         // 字典生成
         ZhiOrder.forEach((v, idx) => {
-            this.#_ganZodiacMap[v] = this.#_zodiac[idx];
+            this.__ganZodiacMap[v] = this.__zodiac[idx];
         });
     }
     /**
@@ -373,7 +388,7 @@ class GanZhi{
      * @returns {array}
      */
     get gOrder(){
-        return this.#_gOrder;
+        return this.__gOrder;
     }
 
     /**
@@ -381,7 +396,7 @@ class GanZhi{
      * @returns {array}
      */
     get zOrder(){
-        return this.#_zOrder;
+        return this.__zOrder;
     }
 
     /**
@@ -389,14 +404,14 @@ class GanZhi{
      * @returns {array}
      */
     get zodiacOrder(){
-        return this.#_zodiac;
+        return this.__zodiac;
     }
     /**
      * 获取干支表
      * @return {array}
      */
     getGZList(){
-        if(this.#_gzList.length == 0){
+        if(this.__gzList.length == 0){
             let que = [], max = 60;
             let order = 0;
             //单配单，双配双
@@ -409,9 +424,9 @@ class GanZhi{
                 que.push(gz);
                 order = que.length;
             }
-            this.#_gzList = que;
+            this.__gzList = que;
         }
-        return this.#_gzList;
+        return this.__gzList;
     }
 
     /**
@@ -426,7 +441,70 @@ class GanZhi{
         gWord = year%10;
         zWord = year%12;
 
-        return `${this.#_gOrder[gWord]}${this.#_zOrder[zWord]}`;
+        return `${this.__gOrder[gWord]}${this.__zOrder[zWord]}`;
+    }
+
+    /**
+     * 通过干支获取对应的年份列表
+     * @param {string|number} gz 年份或者甲子
+     * @param {number} benchmarkYear 基准年（小于等于它的数据列表），默认当年
+     * @param {number} num 生成数量，默认: 20
+     */
+    getGzYearList(gz, benchmarkYear, num){
+        gz = gz? ('number' === typeof gz? this.getYearGz(gz): gz) : gz;
+        gz = gz? gz: this.getYearGz();
+        let gzList = this.getGZList();
+        let checkGzIdx = gzList.indexOf(gz);
+
+        if (checkGzIdx === -1){
+            throw new Error(`${gz} 不是一个有效的干支名称！`);
+            return;
+        }
+        let gzArr = gz.split('');
+        if(gzArr && gzArr.length === 2){
+            let gIdx = this.__gOrder.indexOf(gzArr[0]), 
+                zIdx = this.__zOrder.indexOf(gzArr[1])
+            ;
+            if (gIdx > -1 && zIdx > -1){
+                benchmarkYear = isNaN(benchmarkYear)? 0 : parseInt(benchmarkYear);
+                benchmarkYear = benchmarkYear < 1?  (new Date()).getFullYear(): benchmarkYear;
+
+                let stdGidx = benchmarkYear%10;
+                let stdZidx = benchmarkYear%12;
+                
+                // 如果基准与需要的干支年一样，则其作为基准年
+                let stdYear = 0;    //离基准最近的年份
+                if (stdGidx === gIdx && stdZidx === zIdx){
+                    stdYear = benchmarkYear;
+                }else{
+                    // 通过干支表求出idex索引，再有索引比对得到基准年份
+                    let curGzIdx = gzList.indexOf(this.getYearGz(benchmarkYear));
+                    if(curGzIdx > checkGzIdx){
+                        stdYear = benchmarkYear-(curGzIdx-checkGzIdx);
+                    }else{
+                        stdYear = benchmarkYear+(checkGzIdx-curGzIdx)-GzSpanceYear;
+                    }
+                    
+                }
+
+                num = num? num: 20;
+
+                if (stdYear > 0){
+                    let nC = 0;
+                    let yearList = [stdYear];
+                    num -= 1;
+                    while(nC < num){
+                        stdYear -= GzSpanceYear;
+                        yearList.push(stdYear);
+                        nC += 1;
+                    }
+
+                    return yearList;
+                }
+
+            }
+        }
+        return null;
     }
 
     /**
@@ -439,10 +517,57 @@ class GanZhi{
         }
         let zWord;
         zWord = year%12;
-        let zhi = this.#_zOrder[zWord];
+        let zhi = this.__zOrder[zWord];
 
-        return this.#_ganZodiacMap[zhi];
+        return this.__ganZodiacMap[zhi];
     }
+
+     /**
+     * 通过干支获取对应的年份列表
+     * @param {string|number} zodiac 年份或者甲子
+     * @param {number} benchmarkYear 基准年（小于等于它的数据列表），默认当年
+     * @param {number} num 生成数量，默认: 20
+     */
+    getZodiacList(zodiac, benchmarkYear, num){
+        zodiac = zodiac? ('number' === typeof zodiac? this.zodiac(zodiac): zodiac) : zodiac;
+        zodiac = zodiac? zodiac: this.zodiac();
+        let idx = this.__zodiac.indexOf(zodiac);
+
+        if (idx === -1){
+            throw new Error(`${zodiac} 不是一个有效的生肖名称！`);
+            return;
+        }
+        
+        benchmarkYear = isNaN(benchmarkYear)? 0 : parseInt(benchmarkYear);
+        benchmarkYear = benchmarkYear < 1?  (new Date()).getFullYear(): benchmarkYear;
+
+        let stdIdx = this.__zodiac.indexOf(this.zodiac(benchmarkYear));
+        // 如果基准与需要的干支年一样，则其作为基准年
+        let stdYear = 0;    //离基准最近的年份
+        console.log(idx, stdIdx, this.__zodiac[idx],this.__zodiac[stdIdx]);
+        if(idx <= stdIdx){
+            stdYear = benchmarkYear - (stdIdx - idx);
+        }else{
+            stdYear = benchmarkYear + (idx-stdIdx) - ZodiacSpanceYear;
+        }
+
+        num = num? num: 20;
+
+        if (stdYear > 0){
+            let nC = 0;
+            let yearList = [stdYear];
+            num -= 1;
+            while(nC < num){
+                stdYear -= ZodiacSpanceYear;
+                yearList.push(stdYear);
+                nC += 1;
+            }
+
+            return yearList;
+        }
+        return null;
+    }
+
 }
 
 // --------------------------------------------------------------------------(GZ/end)------------------------------------------------------
