d864957a7830fdb68c0b7ee3cff0c6fe2eacce40
Conero <conero@163.com>
2020-09-13 22:14:28 +0800

    py: patch-rename optimize

--- hi-python/trys/patch-rename.py
+++ hi-python/trys/patch-rename.py
@@ -45,12 +45,15 @@ if __name__ == "__main__":
     # rename_file(vdir, 297, suffix='.jpg', limit=20, prex='p', like='IMG_2020')
     print('欢迎使用批量文件名称修改修改器，根据索引修改，适用于图片等二进制文件')
     print('')
-    vdir = input('清输入目标地址：')
-    index = input_number('清输入索引名称：')
+    vdir = input('清输入目标地址(./)：')
+    if vdir == '' or not vdir:
+        vdir = './'
+    
+    index = input_number('清输入索引名称(数字)：')
     prex = input('清输入前缀：')
     like = input('清输入正则匹配：')
-    limit = input_number('清输入修改数量：')
-    suffix = input('清输入过滤后缀：')
+    limit = input_number('清输入修改数量（数字）：')
+    suffix = input('清输入过滤后缀(如 .jpg)：')
     onlyshow = input('清输入仅仅显示结果，不执行：')
     
     print(f'（目录： {vdir} ， 索引： {index} ， 前缀：{prex} ， 正则过滤：{like} ， 限制数：{limit} ， 后缀：{suffix} ， 不执行：{onlyshow} ？）')
