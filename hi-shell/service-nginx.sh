b068e38fa3be02f67fd84f249c4002c564bc79e5
Joshua Conero <conero@163.com>
2022-02-08 10:48:47 +0800

    shell: nginx 服务输出增加颜色

--- hi-shell/service-nginx.sh
+++ hi-shell/service-nginx.sh
@@ -19,27 +19,24 @@ DESC="nginx 服务器"
 # 服务启动
 start()
 {
-    echo "服务启动中……"
+    echo -e "\033[35m服务启动中…… \033[0m"
     /usr/local/nginx/sbin/nginx
-    echo "[SUCCESS] 服务已启动！"
-    exit 0;
+    echo -e "\033[32m [SUCCESS] \033[0m 服务已启动！"
 }
 
 # 服务停止
 stop()
 {
-    echo "服务停止中……"
+    echo "\033[35m服务停止中…… \033[0m"
     /usr/local/nginx/sbin/nginx -s stop
-    echo "[SUCCESS] 服务已停止！"
-    exit 0;
+    echo -e "\033[32m [SUCCESS] \033[0m 服务已停止！"
 }
 
 reload()
 {
-    echo "服务重载中……"
+    echo "\033[35m服务重载中…… \033[0m"
     /usr/local/nginx/sbin/nginx -s reload
-    echo "[SUCCESS] 服务已重载配置文件！"
-    exit 0;
+    echo -e "\033[32m [SUCCESS] \033[0m 服务已重载配置文件！"
 }
 
 case "$1" in
@@ -57,7 +54,6 @@ restart)
     start
     ;;
 *)
-    echo "usage: $0 start|stop|restart|reload"
-    exit 0;
+    echo -e "\033[36m usage: $0 start|stop|restart|reload \033[0m"
 esac
-exit
+exit 0;
