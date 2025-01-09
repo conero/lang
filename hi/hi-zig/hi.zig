d7027d29afda271ae05f649a1eea17af28c8697f
Joshua Conero <conero@163.com>
2025-01-09 18:50:38 +0800

    zig 备注信息完善

--- hi/hi-zig/hi.zig
+++ hi/hi-zig/hi.zig
@@ -1,8 +1,11 @@
 // zig 实例
 // 2023年4月20日 星期四
-// window 下无 libc 时，使用如下进行编译
-//
+// window 下无 libc 时，使用如下进行编译。
+// --library c   表明不依赖任何系统文件！
 // zig build-exe hi.zig --library c
+//
+// 极小代码编译
+// zig build-exe hi.zig --library c -O ReleaseSmall -fstrip -fsingle-threaded -target x86_64-windows
 
 const std = @import("std");
 const process = std.process;
